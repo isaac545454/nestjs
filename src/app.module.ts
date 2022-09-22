336d32bd020b0b3b0b0c2f86b2fdb71106b3370f
isaac gomes <93602614+isaac545454@users.noreply.github.com>
2022-09-22 18:27:44 -0300

    fim do crud

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://isaac:8tWWycOWLaQ0IMye@cluster0.hjhia.mongodb.net/test',
+    ),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { User, UserDocument } from './entities/user.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.updateOne({ _id: id }, updateUserDto);
+  }
+
+  remove(id: string) {
+    return this.userModel.deleteOne({ _id: id }).exec();
+  }
+}
